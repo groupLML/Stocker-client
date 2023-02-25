cdfc624a5b851c45306a318afbf51f3e83c48b9c
meitavmk96 <meitavmk96@gmail.com>
2023-02-25 12:56:10 +0200

    Adding a list of department types in Global

--- client_app/GlobalData/GlobalData.jsx
+++ client_app/GlobalData/GlobalData.jsx
@@ -9,10 +9,11 @@ export default function GlobalData(props) {
 
     const apiUrlUser = "https://localhost:7102/api/User/";
 
+    const DepTypeList = ["כירורגיה", "פנימית", "אורתופדיה"];
 
     return (
         <GlobalContext.Provider
-        value={{ User, apiUrlUser, setUser }}>
+            value={{ User, apiUrlUser, setUser, DepTypeList}}>
             {props.children}
         </GlobalContext.Provider>
     )
