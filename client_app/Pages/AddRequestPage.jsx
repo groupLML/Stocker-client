96bf9b858b839229f939b4317bf6c210ec1f13b7
linBM1996 <linbenmeir0@gmail.com>
2023-02-27 13:40:47 +0200

    autocomplete-input

--- client_app/Pages/AddRequestPage.jsx
+++ client_app/Pages/AddRequestPage.jsx
@@ -1,14 +1,60 @@
 import React, { useState, useContext, useEffect } from 'react';
-import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, FlatList } from 'react-native';
 import Checkbox from 'expo-checkbox';
 import FCDepTypeList from '../FunctionalComps/FCDepTypeList';
 import { GlobalContext } from '../GlobalData/GlobalData';
 import { AntDesign } from '@expo/vector-icons';
+import Autocomplete from 'react-native-autocomplete-input';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 
 export default function AddRequestPage(props) {
 
+
+  //-----------------------------------Autocomplete med input---------------------------------------------
+  const data = [
+    { id: 1, name: 'Apple' },
+    { id: 2, name: 'Banana' },
+    { id: 3, name: 'Cherry' },
+    { id: 4, name: 'Date' },
+  ];
+
+  const [query, setQuery] = useState('');//tracks the user's input
+  const [selectedItems, setSelectedItems] = useState([]);//stores the selected items
+
+  const filterData = (query) => {//filter the data source based on the user's input
+    if (query === '') {
+      return [];
+    }
+    return data.filter((item) => item.name.toLowerCase().startsWith(query.toLowerCase()));
+  };
+
+  const renderSuggestion = ({ item }) => {//render each auto completion suggestion
+    <TouchableOpacity onPress={() => addItem(item)}>
+      <Text>{item.name}</Text>
+    </TouchableOpacity>
+  };
+
+  const renderSelectedItem = ({ item }) => (//render each selected item's name
+    <View style={styles.selectedItem}>
+      <Text>{item.name}</Text>
+      <TouchableOpacity onPress={() => removeItem(item)}>
+        <Icon name="times" size={20} />
+      </TouchableOpacity>
+    </View>
+  );
+
+  const addItem = (item) => {// add a selected item to the selectedItems array
+    setSelectedItems([...selectedItems, item]);
+    setQuery('');
+  };
+
+  const removeItem = (itemName) => {// remove a selected item from the selectedItems array
+    setSelectedItems(selectedItems.filter((item) => item.name !== itemName));
+  };
+  //-------------------------------------------------------------------------------------------
+
   const [isChecked, setChecked] = useState(true);
-  const [depName, setDepName] = useState('');
   const [reqQty, setReqQty] = useState(0);
   const { DepTypes, setDepTypes, Meds } = useContext(GlobalContext);
 
@@ -56,23 +102,38 @@ export default function AddRequestPage(props) {
       <View>
         <View style={styles.row}>
           <Text style={styles.fields}>שם תרופה:</Text>
-          <TextInput
+          <Autocomplete
+            data={filterData(query)}
+            value={query}
+            onChangeText={(text) => setQuery(text)}
+            renderItem={renderSuggestion}
+            keyExtractor={(item) => item.id.toString()}
+            placeholder="Type here..."
+            containerStyle={styles.autocompleteContainer}
+            listStyle={styles.listStyle} />
+          <FlatList
+            data={selectedItems}
+            renderItem={renderSelectedItem}
+            keyExtractor={(item) => item.id.toString()}
+            style={styles.selectedItemsContainer} />
+
+          {/* <TextInput
             style={styles.input}
             value={depName}
             onChangeText={(text) => setDepName(text)}
-          />
+          /> */}
         </View>
         <View style={styles.row}>
           <Text style={styles.fields}>כמות:</Text>
           <TouchableOpacity onPress={handleIncreaseQuantity}>
             <AntDesign name="plus" size={20} color="black" />
           </TouchableOpacity>
-       
+
           <Text style={{ marginHorizontal: 10, fontSize: 18 }}>{reqQty}</Text>
 
           <TouchableOpacity onPress={handleDecreaseQuantity}>
             <AntDesign name="minus" size={20} color="black" />
-          </TouchableOpacity>          
+          </TouchableOpacity>
         </View>
         <View>
           <View style={styles.row}>
@@ -140,8 +201,8 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontSize: 18,
   },
-/*   quantity: {
-    size: 24,
-    color: "black",
-  }, */
+  /*   quantity: {
+      size: 24,
+      color: "black",
+    }, */
 });
