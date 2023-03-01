ab47f7dc255ce37b771ca6741cf574d3294f8e38
linBM1996 <linbenmeir0@gmail.com>
2023-03-01 14:50:28 +0200

    Update FCRequests.jsx

--- client_app/FunctionalComps/FCRequests.jsx
+++ client_app/FunctionalComps/FCRequests.jsx
@@ -3,28 +3,49 @@ import { View } from 'react-native'
 import FCRequest from './FCRequest'
 
 export default function FCRequests(props) {
-
-    let requestsStr = props.RequestsList.map((request, key) => {
-        return <FCRequest
-            id={request.reqId}
-            date={request.reqDate}
-            time={request.reqTime}
-            genName={request.genName}
-            nurseName={request.nurseName}
-           /*  aNurse={request.aUser} */
-            depName={request.depName}
-           /*  aDep={request.aDep} */
-            reqQty={request.reqQty}
-            reqStatus={request.reqStatus}
-            requestsList={props.RequestsList}
-            isDetailedRequest={props.isDetailedRequest}
-            key={request.reqId}
+    let requestsStr;
+    if (props.isDetailedRequest === true) {
+        requestsStr = props.RequestsList.map((request, key) => {
+            return <FCDetailedRequest
+                id={request.reqId}
+                date={request.reqDate}
+                time={request.reqTime}
+                genName={request.genName}
+                nurseName={request.nurseName}
+                /*  aNurse={request.aUser} */
+                depName={request.depName}
+                /*  aDep={request.aDep} */
+                reqQty={request.reqQty}
+                reqStatus={request.reqStatus}
+                requestsList={props.RequestsList}
+                key={request.reqId}
             />;
-    })
+        })
+    }
+    else {
+        requestsStr = props.RequestsList.map((request, key) => {
+            return <FCRequest
+                id={request.reqId}
+                date={request.reqDate}
+                time={request.reqTime}
+                genName={request.genName}
+                nurseName={request.nurseName}
+                /*  aNurse={request.aUser} */
+                depName={request.depName}
+                /*  aDep={request.aDep} */
+                reqQty={request.reqQty}
+                reqStatus={request.reqStatus}
+                requestsList={props.RequestsList}
+                key={request.reqId}
+            />;
+        })
+    }
+
 
     return (
         <View>
-            {requestsStr}
+            {props.isDetailedRequest && (<FCDetailedRequest />)}
+            {!props.isDetailedRequest && (<FCRequest />)}
         </View>
     )
 }
