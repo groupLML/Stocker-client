fbe987c4c9a537e538b1f1e1f1162c761f69ea57
linBM1996 <linbenmeir0@gmail.com>
2023-03-06 16:42:52 +0200

    Pull Order comps

---
+++ client_app/FunctionalComps/FCPullOrder.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function FCPullOrder() {
+  return (
+    <View>
+      <Text>FCPullOrder</Text>
+    </View>
+  )
+}
---
+++ client_app/Pages/PullOrdersPage.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function PullOrdersPage() {
+  return (
+    <View>
+      <Text>PullOrdersPage</Text>
+    </View>
+  )
+}
