4b202260f7e2270ba507bef51aaf1d83bf3d504f
linBM1996 <linbenmeir0@gmail.com>
2023-03-06 22:23:33 +0200

    pull Order page 2

--- client_app/Pages/AddPullOrderPage.jsx
+++ client_app/Pages/AddPullOrderPage.jsx
@@ -1,16 +1,16 @@
-import { View, Text, TouchableOpacity, ScrollView, StyleSheet, Card } from 'react-native'
+import { View, Text, TouchableOpacity, ScrollView, StyleSheet } from 'react-native'
 import React from 'react'
-import { Icon } from 'react-native-elements';
 import FCMedInOrder from '../FunctionalComps/FCMedInOrder';
 
 export default function AddPullOrderPage() {
+
     return (
-        <View style={{ flex: 1 }}><Text>AddPullOrderPage</Text>
+        <View style={{ flex: 1 }}><Text>Add Pull Order:</Text>
             <ScrollView>
-                <FCMedInOrder />
+                <FCMedInOrder/>
             </ScrollView>
-            <TouchableOpacity style={styles.AddBTN}  onPress={() => AddMed()}>
-                <Icon name='add' color='white' />
+            <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}>שליחה</Text>
             </TouchableOpacity>
         </View>
     )
@@ -24,5 +24,15 @@ const styles = StyleSheet.create({
         bottom: 0,
         marginBottom: 20,
         alignSelf: 'center'
+    },
+    button: {
+        backgroundColor: '#00317D',
+        padding: 10,
+        borderRadius: 5,
+        alignItems:'center',
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 16,
     },
 });
