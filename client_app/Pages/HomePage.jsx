2c85eb6bcaa9b66de7d28e7d4c4dc8b3e4933744
linBM1996 <linbenmeir0@gmail.com>
2023-03-16 15:05:34 +0200

    adding home page navigations

--- client_app/Pages/HomePage.jsx
+++ client_app/Pages/HomePage.jsx
@@ -1,13 +1,19 @@
-import { View, Text } from 'react-native';
+import { View, Text, TouchableHighlight, TouchableOpacity } from 'react-native';
 import React from 'react';
 import { useNavigation } from '@react-navigation/native';
 
 export default function HomePage(props) {
   const navigation = useNavigation();
-  
+
   return (
     <View>
       <Text>Home</Text>
+      <TouchableOpacity onPress={() => navigation.navigate('יצירת בקשה')}>
+        <Text>יצירת בקשה</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate('יצירת הזמנת משיכה')}>
+        <Text>יצירת הזמנה חדשה</Text>
+      </TouchableOpacity>
     </View>
   )
 }
