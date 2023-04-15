e92afabeedc9ed895506bd1427371360391f45c5
linBM1996 <linbenmeir0@gmail.com>
2023-04-15 15:56:42 +0300

    Update HomePage.jsx

--- client_app/Pages/HomePage.jsx
+++ client_app/Pages/HomePage.jsx
@@ -87,8 +87,7 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
-    paddingHorizontal: 30,
+    alignSelf: 'center',
     marginTop: 20,
   },
   button: {
