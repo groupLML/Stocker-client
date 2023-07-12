fd7a7bee4b129657a9308b89d230617ee84425bc
linBM1996 <linbenmeir0@gmail.com>
2023-07-12 18:04:17 +0300

    MyRequestPage

--- client_app/FunctionalComps/FCstaticDepTypeList.jsx
+++ client_app/FunctionalComps/FCstaticDepTypeList.jsx
@@ -4,53 +4,40 @@ import React, { useState, useContext, useEffect } from 'react';
 import { GlobalContext } from '../GlobalData/GlobalData';
 import { useFocusEffect } from '@react-navigation/native';
 
-export default function FCDepTypeList(props) {
+
+export default function FCstaticDepTypeList(props) {
 
     //-----------------------------Dep Type List------------------------------------
     const [isChecked, setChecked] = useState(null); //Send to all deps checkBox status (init value is checked)
+    const [selectedDepTypes, setSelectedDepTypes] = useState(props.ReqDeps);
+    const [didupdateDepTypes, setDidupdateDepTypes] = useState(props.ReqDeps);
     const { DepTypes, setDepTypes } = useContext(GlobalContext);
 
-/*     useEffect(() => {
-        console.log('ReqDeps', props.ReqDeps);
-        if (props.ReqDeps !== undefined) {
-          setDepTypes(props.ReqDeps);
-        }
-      }, []); */
-
-    /*     const setInitDeps = (Deps) => {
-            setDepTypes(Deps);
-            console.log('here', DepTypes)
-        }; */
-
-    const handleChange = (ChosenDepName) => { //A specific dep type checkbox is checked
-        let temp = DepTypes.map((DepType) => { //find the Chosen Dep and add to temp array
-            if (ChosenDepName === DepType.name) {
-                return { ...DepType, isChecked: !DepType.isChecked };
-            }
-            return DepType;
-        });
-        setDepTypes(temp);//set the depTypes array for temp
-    };
-
     useEffect(() => {
-        console.log('DepTypes:', DepTypes);
-        if (DepTypes.every((item) => item.isChecked === true)) {
+        if (selectedDepTypes.every((item) => item.isChecked === true)) {
             setChecked(true);
         }
         else {
             setChecked(false);
         }
-    }, [DepTypes]);
+        setDepTypes(selectedDepTypes);
+        console.log("DepTypes", DepTypes);
+    }, [selectedDepTypes]);
+
+    const handleChange = (ChosenDepName) => { //A specific dep type checkbox is checked
+        let temp = selectedDepTypes.map((DepType) => { //find the Chosen Dep and add to temp array
+            if (ChosenDepName === DepType.name) {
+                return { ...DepType, isChecked: !DepType.isChecked };
+            }
+            return DepType;
+        });
+        setSelectedDepTypes(temp);//set the depTypes array for temp
+    };
 
     useFocusEffect(
-        React.useCallback(() => {
-            setChecked(true); // Reset the isChecked state to its initial value
-            const initDepTypes = DepTypes.map((depType) => ({// Reset the isChecked state of DepTypes to their initial value -> true
-                ...depType,
-                isChecked: true,
-            }));
-            setDepTypes(initDepTypes);
 
+        React.useCallback(() => {
+            setSelectedDepTypes(props.ReqDeps); // Reset the isChecked state to its initial value
             return () => {
                 // Clean up the effect when the screen goes out of focus
             };
@@ -65,7 +52,7 @@ export default function FCDepTypeList(props) {
             </View >
             <View style={styles.depsCB}>
                 {!isChecked && <FlatList //when Send to all deps checkBox status is false (unchecked) show all checkboxs options
-                    data={DepTypes}
+                    data={selectedDepTypes}
                     renderItem={({ item }) => (
                         <View style={styles.row}>
                             <Checkbox style={styles.CB} color={item.isChecked ? '#003D9A' : undefined} value={item.isChecked} onValueChange={() => handleChange(item.name)} />
--- client_app/Pages/MyRequestPage.jsx
+++ client_app/Pages/MyRequestPage.jsx
@@ -1,17 +1,21 @@
 import { View, Text, StyleSheet } from 'react-native';
-import React from 'react';
-
+//import React from 'react';
+import React, { useEffect } from 'react';
 import FCMyRequests from '../FunctionalComps/FCMyRequests';
 
 export default function MyRequestPage(props) {
 
   const { requestId, requestsList, ReqDeps } = props.route.params;
 
+  useEffect(() => {
+    console.log("ReqDeps:", ReqDeps);
+  }, []);
+
   let request = requestsList.filter((item) => item.reqId === requestId);//get the request item to read 
 
   return (
     <View style={styles.container}>
-      <FCMyRequests RequestsList={request} isDetailedRequest={true} ReqDeps={ReqDeps}/>
+      <FCMyRequests RequestsList={request} isDetailedRequest={true} ReqDeps={ReqDeps} />
     </View>
   );
 }
